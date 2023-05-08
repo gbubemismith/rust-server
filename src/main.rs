4529eacbeaad2e5e09f9dd1ab09d587f78278b61
gbubemismith <gsmithwalter@gmail.com>
2023-05-07 23:50:56 -0400

    Initial code structure and definitions

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+struct Server {
+    address: String,
 }
+
+impl Server {
+    fn new(addr: &str) -> Self {
+        Self {
+            address: addr.to_owned(),
+        }
+    }
+
+    fn run(&self) {}
+}
+
+fn main() {}
